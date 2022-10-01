ae6fb651ce89b8ae56fce7a70313dc9951daf254
AishwariyaRanjan <ranjanaishwariya107@gmail.com>
2022-09-30 23:36:28 -0700

    got error "No adapter attached, skipping layout"

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.flixster
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        val supportFragmentManager = supportFragmentManager
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.content, MovieFragment(), null)
+
     }
 }
---
+++ app/src/main/java/com/example/flixster/Movie.kt
@@ -0,0 +1,15 @@
+package com.example.flixster
+
+import com.google.gson.annotations.SerializedName
+
+class Movie {
+    @SerializedName("title")
+    var title: String? = null
+
+    @SerializedName("poster_path")
+    var poster_path: String? = null
+
+    @SerializedName("overview")
+    var overview: String? = null
+
+}
